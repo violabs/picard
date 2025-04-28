56534bdee2b22708b8661e85716744371bd747b9
Violabs Josh <139578299+violabs-josh@users.noreply.github.com>
2025-04-27 22:56:18 -0500

    Resource quota

---
+++ core/src/test/kotlin/io/violabs/picard/domain/k8sResources/policy/resourceQuota/ResourceQuotaListTest.kt
@@ -0,0 +1,38 @@
+package io.violabs.picard.domain.k8sResources.policy.resourceQuota
+
+
+import io.violabs.picard.FullBuildSim
+import io.violabs.picard.possibilities
+import org.junit.jupiter.api.BeforeAll
+
+class ResourceQuotaListTest : FullBuildSim<ResourceQuotaList, ResourceQuotaList.Builder>() {
+    companion object {
+        @JvmStatic
+        @BeforeAll
+        fun setup() = buildSetup(
+            ResourceQuotaListTest::class,
+            SUCCESS_POSSIBILITIES,
+            FAILURE_POSSIBILITIES
+        )
+
+        private val SUCCESS_POSSIBILITIES = possibilities<ResourceQuotaList, ResourceQuotaList.Builder> {
+            scenario {
+                id = "minimum"
+                given(ResourceQuotaList.Builder()) {
+                    items {
+                        quota {  }
+                    }
+                }
+                expected = ResourceQuotaList(
+                    items = listOf(ResourceQuota())
+                )
+            }
+        }
+
+        private val FAILURE_POSSIBILITIES = possibilities<ResourceQuotaList, ResourceQuotaList.Builder> {
+            requireNotEmptyScenario("items") {
+                given(ResourceQuotaList.Builder())
+            }
+        }
+    }
+}

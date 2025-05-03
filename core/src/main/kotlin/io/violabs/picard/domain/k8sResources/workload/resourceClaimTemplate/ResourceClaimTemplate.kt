857bf0ef0fcddccbd32a41a19d070267580121c0
Violabs Josh <139578299+violabs-josh@users.noreply.github.com>
2025-05-03 13:52:03 -0500

    Resource Claim and Resource Claim Template

--- core/src/main/kotlin/io/violabs/picard/domain/k8sResources/workload/resourceClaimTemplate/ResourceClaimTemplate.kt
+++ core/src/main/kotlin/io/violabs/picard/domain/k8sResources/workload/resourceClaimTemplate/ResourceClaimTemplate.kt
@@ -1,10 +1,12 @@
 package io.violabs.picard.domain.k8sResources.workload.resourceClaimTemplate
 
+import io.violabs.picard.common.vRequireNotNull
 import io.violabs.picard.domain.*
 import io.violabs.picard.domain.k8sResources.APIVersion
 import io.violabs.picard.domain.k8sResources.K8sResource
 import io.violabs.picard.domain.k8sResources.KAPIVersion
 import io.violabs.picard.domain.BaseSpec
+import io.violabs.picard.domain.k8sResources.K8sListResource
 import io.violabs.picard.domain.k8sResources.workload.resourceClaim.ResourceClaim
 
 data class ResourceClaimTemplate(
@@ -17,5 +19,40 @@ data class ResourceClaimTemplate(
     data class Spec(
         val spec: ResourceClaim.Spec,
         val metadata: ObjectMetadata? = null
-    ) : BaseSpec
+    ) : BaseSpec {
+        class Builder : DSLBuilder<Spec> {
+            private var spec: ResourceClaim.Spec? = null
+            private var metadata: ObjectMetadata? = null
+
+            fun resourceClaimSpec(scope: ResourceClaim.Spec.Builder.() -> Unit) {
+                spec = ResourceClaim.Spec.Builder().apply(scope).build()
+            }
+
+            fun metadata(scope: ObjectMetadata.Builder.() -> Unit) {
+                metadata = ObjectMetadata.Builder().apply(scope).build()
+            }
+
+            override fun build(): Spec {
+                return Spec(
+                    spec = vRequireNotNull(this::spec),
+                    metadata = metadata
+                )
+            }
+        }
+    }
+
+    class Builder : ResourceSpecDSLBuilder<ResourceClaimTemplate, Spec, Spec.Builder>(Spec.Builder()) {
+        override fun build(): ResourceClaimTemplate {
+            return ResourceClaimTemplate(
+                spec = vRequireNotNull(this::spec),
+                metadata = metadata
+            )
+        }
+    }
+
+    class Group : K8sListResource.ItemGroup<ResourceClaimTemplate, Builder>(Builder()) {
+        fun resourceClaimTemplate(scope: Builder.() -> Unit) {
+            item(scope)
+        }
+    }
 }
--- core/src/main/kotlin/io/violabs/picard/domain/k8sResources/workload/resourceClaimTemplate/ResourceClaimTemplateList.kt
+++ core/src/main/kotlin/io/violabs/picard/domain/k8sResources/workload/resourceClaimTemplate/ResourceClaimTemplateList.kt
@@ -1,6 +1,8 @@
 package io.violabs.picard.domain.k8sResources.workload.resourceClaimTemplate
 
+import io.violabs.picard.common.vRequireNotEmpty
 import io.violabs.picard.domain.ListMeta
+import io.violabs.picard.domain.ResourceListDSLBuilder
 import io.violabs.picard.domain.k8sResources.APIVersion
 import io.violabs.picard.domain.k8sResources.K8sListResource
 import io.violabs.picard.domain.k8sResources.KAPIVersion
@@ -11,4 +13,19 @@ data class ResourceClaimTemplateList(
     override val metadata: ListMeta? = null
 ) : K8sListResource<ResourceClaimTemplateList.Version, ResourceClaimTemplate> {
     interface Version : APIVersion
+
+    class Builder : ResourceListDSLBuilder<
+        ResourceClaimTemplate,
+        ResourceClaimTemplate.Builder,
+        ResourceClaimTemplate.Group,
+        ResourceClaimTemplateList
+        >(ResourceClaimTemplate.Group()) {
+
+        override fun build(): ResourceClaimTemplateList {
+            return ResourceClaimTemplateList(
+                items = vRequireNotEmpty(this::items),
+                metadata = metadata
+            )
+        }
+    }
 }

28ee027e88bab7af326535255011d59e45361e97
Violabs Josh <139578299+violabs-josh@users.noreply.github.com>
2025-05-05 22:30:25 -0500

    Add some publishing information

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,4 +1,11 @@
 
+plugins {
+    `maven-publish`
+}
+
+group = "io.violabs.picard"
+version = "0.1.0"
+
 dependencies {
     implementation(project(":common"))
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.19.0")
@@ -8,6 +15,17 @@ dependencies {
     testImplementation("com.fasterxml.jackson.core:jackson-core:2.18.3")
     testImplementation("com.fasterxml.jackson.core:jackson-databind:2.18.3")
     testImplementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.3")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("local") {
+            from(components["java"])
+            groupId    = "io.violabs.picard"
+            artifactId = "core"
+            version    = version
+        }
+    }
 }
 
 kover {

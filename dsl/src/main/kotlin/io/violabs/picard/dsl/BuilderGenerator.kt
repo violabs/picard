3b10845e0b545d3354c654cb2e16f41dc130ea0d
Violabs Josh <139578299+violabs-josh@users.noreply.github.com>
2025-05-11 16:57:35 -0500

    Working generator

--- dsl/src/main/kotlin/io/violabs/picard/dsl/BuilderGenerator.kt
+++ dsl/src/main/kotlin/io/violabs/picard/dsl/BuilderGenerator.kt
@@ -5,13 +5,105 @@ import com.google.devtools.ksp.processing.Dependencies
 import com.google.devtools.ksp.processing.KSPLogger
 import com.google.devtools.ksp.processing.Resolver
 import com.google.devtools.ksp.symbol.KSClassDeclaration
+import com.google.devtools.ksp.symbol.KSPropertyDeclaration
 import com.squareup.kotlinpoet.*
 import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
 import com.squareup.kotlinpoet.ksp.toClassName
 import com.squareup.kotlinpoet.ksp.toTypeName
 import com.squareup.kotlinpoet.ksp.writeTo
 
+interface DSLParam {
+    val propName: String
+    val propTypeName: TypeName
+    fun toPropertySpec(): PropertySpec
+    fun accessors(): List<FunSpec> { return emptyList() }
+}
+
+class BooleanParam(
+    override val propName: String,
+    nullable: Boolean = true
+) : DSLParam {
+    override val propTypeName: TypeName = BOOLEAN.copy(nullable)
+
+    override fun toPropertySpec(): PropertySpec = PropertySpec.builder(propName, propTypeName)
+        .addModifiers(KModifier.PRIVATE)
+        .mutable(true)
+        .initializer("null")
+        .build()
+
+    override fun accessors(): List<FunSpec> {
+        val param = ParameterSpec
+            .builder("on", propTypeName.copy(nullable = false))
+            .defaultValue("true")
+            .build()
+
+        return FunSpec.builder(propName)
+            .addParameter(param)
+            .addStatement("this.%N = %N", propName, param)
+            .build()
+            .let { listOf(it) }
+    }
+}
+
+class DefaultParam(
+    override val propName: String,
+    override val propTypeName: TypeName = STRING
+) : DSLParam {
+    override fun toPropertySpec(): PropertySpec = PropertySpec.builder(propName, propTypeName)
+        .mutable(true)
+        .initializer("null")
+        .build()
+}
+
+class BuilderParam(
+    override val propName: String,
+    override val propTypeName: TypeName
+) : DSLParam {
+    override fun toPropertySpec(): PropertySpec = PropertySpec.builder(propName, propTypeName)
+        .mutable(true)
+        .initializer("null")
+        .build()
+}
+
+class ListParam(
+    override val propName: String,
+    override val propTypeName: TypeName = LIST.parameterizedBy(STRING)
+) : DSLParam {
+    override fun toPropertySpec(): PropertySpec = PropertySpec.builder(propName, propTypeName)
+        .mutable(true)
+        .initializer("null")
+        .build()
+}
+
+class GroupParam(
+    override val propName: String,
+    override val propTypeName: TypeName
+) : DSLParam {
+    override fun toPropertySpec(): PropertySpec = PropertySpec.builder(propName, propTypeName)
+        .mutable(true)
+        .initializer("null")
+        .build()
+}
+
+private val DEFAULT_TYPES = listOf(BOOLEAN, STRING, INT, LONG, SHORT, BYTE, DOUBLE, FLOAT)
+private val DEFAULT_TYPE_NAMES = DEFAULT_TYPES.map { it.simpleName }
+
+fun determineParam(prop: KSPropertyDeclaration): DSLParam {
+    val propClassName = prop.type.resolve().toClassName().simpleName
+    val propName = prop.simpleName.asString()
+    val propTypeName = prop.type.toTypeName().copy(nullable = true)
+
+    return when {
+        propClassName == BOOLEAN.simpleName -> BooleanParam(propName)
+        propClassName in DEFAULT_TYPE_NAMES -> DefaultParam(propName, propTypeName)
+        LIST.simpleName in propClassName -> ListParam(propName, propTypeName)
+        "Group" in propClassName -> GroupParam(propName, propTypeName)
+        else -> BuilderParam(propName, propTypeName)
+    }
+}
+
 class BuilderGenerator(val logger: KSPLogger) {
+
     fun generate(resolver: Resolver, codeGenerator: CodeGenerator, options: Map<String, String> = emptyMap()) {
         val dslBuilderClasspath = options["dslBuilder.classpath"]
 
@@ -51,21 +143,26 @@ class BuilderGenerator(val logger: KSPLogger) {
                 val propName = prop.simpleName.asString()
                 val propTypeName = prop.type.toTypeName().copy(nullable = true)
 
+                val dslParam = determineParam(prop)
+
+                val accessor = if (prop.type.resolve().declaration.simpleName.asString().contains("Boolean"))
+                    KModifier.PRIVATE
+                else
+                    KModifier.PUBLIC
+
                 // a) backing var
-                builderClass.addProperty(
-                    PropertySpec.builder(propName, propTypeName)
-                        .mutable(true)
-                        .initializer("null")
-                        .build()
-                )
+                builderClass.addProperty(dslParam.toPropertySpec())
 
                 // b) DSL setter
-                builderClass.addFunction(
-                    FunSpec.builder(propName)
-                        .addParameter(propName, propTypeName)
-                        .addStatement("this.%N = %N", propName, propName)
-                        .build()
-                )
+                dslParam.accessors().forEach {
+                    builderClass.addFunction(it)
+                }
+//                builderClass.addFunction(
+//                    FunSpec.builder(propName)
+//                        .addParameter(propName, propTypeName)
+//                        .addStatement("this.%N = %N", propName, propName)
+//                        .build()
+//                )
 
                 // c) require-not-null in build()
                 buildFun.addStatement(
@@ -100,7 +197,7 @@ class BuilderGenerator(val logger: KSPLogger) {
             // 6) write into a file named `${TypeName}Dsl.kt`
             FileSpec.builder(pkg, "${typeName}Dsl")
                 .addType(builderClass.build())
-                .addFunction(dslFun)
+//                .addFunction(dslFun)
                 .build()
                 .writeTo(codeGenerator, Dependencies(false))
         }

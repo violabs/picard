859c4c82735a6357215ea2ad7f705aa1849cd8c2
Violabs Josh <139578299+violabs-josh@users.noreply.github.com>
2025-05-20 18:33:39 -0500

    Add missing properties to GRPCClient and Profiling

---
+++ star-charts/loki/src/main/kotlin/io/violabs/picard/starCharts/loki/grpcClient/backoffConfig/BackoffConfig.kt
@@ -0,0 +1,26 @@
+package io.violabs.picard.starCharts.loki.grpcClient.backoffConfig
+
+import io.violabs.picard.dsl.annotation.GeneratedDSL
+import io.violabs.picard.starCharts.loki.Duration
+
+@GeneratedDSL
+data class BackoffConfig(
+    /**
+     *   # Minimum delay when backing off.
+     *   # CLI flag: -<prefix>.backoff-min-period
+     *   [min_period: <duration> | default = 100ms]
+     */
+    val minPeriod: Duration? = null,
+    /**
+     *   # Maximum delay when backing off.
+     *   # CLI flag: -<prefix>.backoff-max-period
+     *   [max_period: <duration> | default = 10s]
+     */
+    val maxPeriod: Duration? = null,
+    /**
+     *   # Number of times to backoff and retry before failing.
+     *   # CLI flag: -<prefix>.backoff-retries
+     *   [max_retries: <int> | default = 10]
+     */
+    val maxRetries: Int? = null,
+)

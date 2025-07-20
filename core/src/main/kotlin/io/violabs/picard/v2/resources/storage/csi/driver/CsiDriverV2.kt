0093812dcaf08e15fb9ea45a270a59fd0866df77
Violabs Josh <139578299+violabs-josh@users.noreply.github.com>
2025-07-20 10:12:24 -0500

    CsiDriverV2 without tests

--- core/src/main/kotlin/io/violabs/picard/v2/resources/storage/csi/driver/CsiDriverV2.kt
+++ core/src/main/kotlin/io/violabs/picard/v2/resources/storage/csi/driver/CsiDriverV2.kt
@@ -25,10 +25,15 @@ data class CsiDriverV2(
     )
     override val apiVersion: Version = KAPIVersion.StorageV1,
     /**
-     * Standard object's metadata.
+     * Standard object metadata. metadata.Name indicates the name of the
+     * CSI driver that this object refers to; it MUST be the same name returned by
+     * the CSI GetPluginName() call for that driver. The driver name must be 63
+     * characters or less, beginning and ending with an alphanumeric character
+     * ([a-z0-9A-Z]) with dashes (-), dots (.), and alphanumerics between.
      * More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#metadata
      */
-    override val metadata: ObjectMeta? = null
+    override val metadata: ObjectMeta? = null,
+    val spec: CsiDriverSpec? = null
 ) : ConfigResource<CsiDriverV2.Version, ObjectMeta> {
     interface Version : APIVersion
 }
